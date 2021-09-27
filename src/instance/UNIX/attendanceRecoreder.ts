2079ebbc8fbd83b515a6794ca53246408764eb87
airRnot1106 <yngw.rintaro@gmail.com>
2021-09-27 10:41:17 +0900

    出席登録を追加

--- src/instance/UNIX/attendanceRecoreder.ts
+++ src/instance/UNIX/attendanceRecoreder.ts
@@ -18,5 +18,28 @@ export default class AttendanceRecorder implements IFunc {
     await Promise.all([Browser.page?.waitForNavigation(), link?.click()]);
     const url = Browser.page?.url();
     await Browser.page?.goto(`${url}&view=5`);
+    const statusCol = await Browser.page?.$('td.statuscol');
+    const sendButton = await statusCol?.$x(
+      './/a[contains(text(), "出欠を送信する")]'
+    );
+    if (!sendButton![0]) {
+      console.log(
+        chalk.yellow(
+          'You have already attended or your attendance period has expired'
+        )
+      );
+      process.exit(0);
+    }
+    await Promise.all([
+      Browser.page?.waitForNavigation(),
+      sendButton![0].click(),
+    ]);
+    const radio = await Browser.page?.$('.form-check-input');
+    await radio?.click();
+    const storeButton = await Browser.page?.$('#id_submitbutton');
+    await Promise.all([
+      Browser.page?.waitForNavigation(),
+      storeButton?.click(),
+    ]);
   }
 }
