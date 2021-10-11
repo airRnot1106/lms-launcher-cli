fdc1b7fa0bc12b84d54fe062f464e8fc6182cf2a
airRnot1106 <yngw.rintaro@gmail.com>
2021-10-11 10:38:06 +0900

    fix member 'selecter' -> selector

--- src/controller/recordAttendanceController.ts
+++ src/controller/recordAttendanceController.ts
@@ -11,22 +11,22 @@ import { IFunc } from 'iFunc';
 export default class RecordAttendanceController implements IFunc {
   private _login: Login;
   private _classSearcher: ClassSearcher;
-  private _classSelecter: ClassSelector;
-  private _sectionSelecter: SectionSelector;
+  private _classSelector: ClassSelector;
+  private _sectionSelector: SectionSelector;
   private _attendanceRecorder: AttendanceRecorder;
   constructor() {
     this._login = new Login();
     this._classSearcher = new ClassSearcher();
-    this._classSelecter = new ClassSelector();
-    this._sectionSelecter = new SectionSelector();
+    this._classSelector = new ClassSelector();
+    this._sectionSelector = new SectionSelector();
     this._attendanceRecorder = new AttendanceRecorder();
   }
   async execute() {
     await Browser.initialize(true);
     await this._login.execute();
     const classNames = await this._classSearcher.execute();
-    await this._classSelecter.execute(classNames);
-    await this._sectionSelecter.execute();
+    await this._classSelector.execute(classNames);
+    await this._sectionSelector.execute();
     await this._attendanceRecorder.execute();
   }
 }
--- src/controller/recordAttendanceControllerWin.ts
+++ src/controller/recordAttendanceControllerWin.ts
@@ -11,22 +11,22 @@ import { IFunc } from 'iFunc';
 export default class RecordAttendanceControllerWin implements IFunc {
   private _login: Login;
   private _classSearcherWin: ClassSearcherWin;
-  private _classSelecterWin: ClassSelectorWin;
-  private _sectionSelecterWin: SectionSelectorWin;
+  private _classSelectorWin: ClassSelectorWin;
+  private _sectionSelectorWin: SectionSelectorWin;
   private _attendanceRecorder: AttendanceRecorder;
   constructor() {
     this._login = new Login();
     this._classSearcherWin = new ClassSearcherWin();
-    this._classSelecterWin = new ClassSelectorWin();
-    this._sectionSelecterWin = new SectionSelectorWin();
+    this._classSelectorWin = new ClassSelectorWin();
+    this._sectionSelectorWin = new SectionSelectorWin();
     this._attendanceRecorder = new AttendanceRecorder();
   }
   async execute() {
     await Browser.initialize(true);
     await this._login.execute();
     const classNames = await this._classSearcherWin.execute();
-    await this._classSelecterWin.execute(classNames);
-    await this._sectionSelecterWin.execute();
+    await this._classSelectorWin.execute(classNames);
+    await this._sectionSelectorWin.execute();
     await this._attendanceRecorder.execute();
   }
 }
